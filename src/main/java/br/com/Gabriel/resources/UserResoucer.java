00451e04a317c90d50e42dc0911c5cd335b2df1a
gabrielfurtadon <gabrielfurtado@alunos.utfpr.edu.br>
2023-04-24 12:22:16 -0300

    User Delete

--- src/main/java/br/com/Gabriel/resources/UserResoucer.java
+++ src/main/java/br/com/Gabriel/resources/UserResoucer.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -48,6 +49,13 @@ public class UserResoucer {
 		return ResponseEntity.created(uri).body(obj);
 	}
 	
+	
+	//DELETAR USER     void pois a resposta da requisição não vai retornar nenhum corpo
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 	 
 	
 	
