3bc2de007ea70a9f97ab7175d57e0cef609b065c
gabrielfurtadon <gabrielfurtado@alunos.utfpr.edu.br>
2023-04-24 15:02:05 -0300

    Update User

--- src/main/java/br/com/Gabriel/services/UserService.java
+++ src/main/java/br/com/Gabriel/services/UserService.java
@@ -33,4 +33,17 @@ public class UserService {
 		repository.deleteById(id);
 	}
 	
+	public User update(Long id, User obj) {
+		User entity = repository.getReferenceById(id); //INSTANCIAR O USUARIO PELO JPA, SEM IR NO BANCO AINDA
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+		
+	}
+	
 }
